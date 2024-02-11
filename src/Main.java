105bf547d815646a9a2e7d4a7ac9647d49ea8bb2
ImRaabe <raabees@mail.uc.edu>
2024-02-11 14:46:29 -0500

    Project init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter the number of your birth month(1-12): ");
+        int birthMonth = in.nextInt();
+        if(birthMonth <= 12 && birthMonth >= 1)
+        {
+            System.out.println("Your birth month is: " + birthMonth);
+        }
+        else {
+            System.out.println("You entered an incorrect month value: " + birthMonth);
+        }
     }
 }
